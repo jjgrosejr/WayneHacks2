ea91defc38cbdf55e4668fc088cbc5a591b14955
ArchAssassin <72637685+jjgrosejr@users.noreply.github.com>
2024-01-13 15:19:53 -0500

    on

--- instagram.py
+++ instagram.py
@@ -12,6 +12,7 @@ username = "hoebottest"
 password = "Homie-144"
 
 cl = Client()
+
 cl.login(username, password)
 
 message = input("please enter message: ")
@@ -20,12 +21,8 @@ numRecipients = input("how many recipients would you like to message: ")
 
 list = []
 for i in range(int(numRecipients)):
-    recipient = input("Please enter recipient username: ")
-    list.append(cl.user_id_from_username(recipient))
+    recipient = input("please enter recipient username: ")
+    list.append(int(cl.user_id_from_username(recipient)))
 
 cl.direct_send(message, list)
 
-""" messageList = cl.direct_messages(cl.direct_thread_by_participants(list))
-
-print(messageList[0])
- """
