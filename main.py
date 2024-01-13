2fcc325655ce2e911e89f52bb5632922699b861c
ArchAssassin <72637685+jjgrosejr@users.noreply.github.com>
2024-01-13 14:10:01 -0500

    iuuhu

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import openai
 import tkinter as tk
 from instagrapi import Client
+from instagrapi import DirectMixin
 
 openai.my_api_key = 'org-tIGaYoDJjUPeawEvfqE0QUmD'
 
@@ -21,6 +22,10 @@ list = []
 for i in range(int(numRecipients)):
     recipient = input("Please enter recipient username: ")
     list.append(cl.user_id_from_username(recipient))
- 
+
 cl.direct_send(message, list)
 
+""" messageList = cl.direct_messages(cl.direct_thread_by_participants(list))
+
+print(messageList[0])
+ """
